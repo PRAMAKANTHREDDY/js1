281cd883d33984658d50a1f958e3e8ff84331872
PRAMAKANTHREDDY <74037226+PRAMAKANTHREDDY@users.noreply.github.com>
2022-03-23 14:03:41 +0530

    Add files via upload

---
+++ script.js
@@ -0,0 +1,16 @@
+var r
+r = 3
+var flo = 3.14
+let ra
+var rad = "area of circle"
+console.log(rad)
+ra = r * r * flo
+
+console.log(ra)
+const cr = flo * 2 * r
+var rad = "circumference of circle"
+console.log(rad)
+console.log(cr)
+var rad = "radius of circle"
+console.log(rad)
+console.log(r)
